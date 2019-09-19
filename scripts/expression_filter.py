41b82b8e754c0872934f922e288f9f048761006e
SGMartin <s.g.martin94@gmail.com>
2019-09-19 18:15:57 +0200

    stubs for later pandrugs annot.

---
+++ scripts/expression_filter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Expression filter module: This script
+'''
+
+import sys
+
+import pandas as pd
+
+def main():
+	'''
+	'''
+
+
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])	
---
+++ scripts/pandrugs_annotation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Section: SL in TCGA data
+
+Purpose: Annotates filtered alterations used PanDrugs database.
+"""
+
+import sys
+
+import pandas as pd
+import requests
+
+def main(summary: pd.DataFrame):
+	'''
+	'''
+
+
+
+
+if __name__ == "__main__":
+	main()
