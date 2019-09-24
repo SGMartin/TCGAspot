e580b6373cd2476e28fc630bf67159c44bcafa77
SGMartin <s.g.martin94@gmail.com>
2019-09-24 18:23:01 +0200

    vulcan plots dev.

--- scripts/vulcan_drugs_plots.py
+++ scripts/vulcan_drugs_plots.py
@@ -37,13 +37,40 @@ def main(summary: str, vulcan_treatments_db: str):
 															   'has_lincs'
 															  ].sum()
 
-	tcga_drug_summary 		  = pd.DataFrame(tcga_drug_summary).reset_index()
-	tcga_drug_summary.columns = ['project',
-								 'pandrugs_based_local',
-								 'lincs_based_local'
-								]
+	tcga_drug_summary 		  = pd.DataFrame(data=tcga_drug_summary,
+											 columns=['pandrugs_based_local',
+											 		  'lincs_based_local']
+											)
+
 	
+	# delete this object to free allocated mem.
+	del drugs_local_summary
+
+	drugs_pancancer_summary = pd.merge(left=drug_summary[drug_summary['Vulcan_Pancancer']],
+									   right=vulcan_drugs_db[vulcan_drugs_db['tissue'] == 'PANCANCER'],
+									   how='left',
+									   left_on=['Hugo_Symbol', 'Consequence'],
+									   right_on=['geneA', 'alteration']
+									   )
 	
+
+	drugs_pancancer_summary['has_dscore'] = drugs_pancanser_summary['Dscore'] > 0
+	drugs_pancancer_summary['has_lincs']  = drugs_pancancer_summary['LINCS'] > 0
+
+	tcga_drug_pancancer_summary = drugs_pancancer_summary.groupby('Project')['has_dscore', 'has_lincs'].sum()
+
+	del drugs_pancancer_summary
+
+	tcga_drug_pancancer_summary = pd.DataFrame(data=tcga_drug_pancancer_summary,
+											   columns=['pandrugs_based_pancancer',
+											   			'lincs_based_pancancer'
+													   ]
+											   )
+
+
+
+
+
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
