4fcf5f8e81bb74a4704526d7cbe3cdeb82ccef91
SGMartin <s.g.martin94@gmail.com>
2019-12-12 14:54:19 +0100

    Fixed no cnv to test crash in dif.expr.

--- scripts/cnv_diff_expr.R
+++ scripts/cnv_diff_expr.R
@@ -81,6 +81,19 @@ perform_deseq2 <- function(data_matrix, samples, controls, gene){
     return (c(p_value, log2FC))
 }
 
+save_dummy_dataframe <-function(target)
+{
+    # Generating empty dataframe for now, so it won't crash downstream
+    empty_data <- data.frame('Hugo_Symbol'= character(), 
+                             'adj_pval'   = character(),
+                             'log2fc'     = character(),
+                             'method'     = character(),
+                             'cases'      = character(),
+                             'bias'       = character())
+    
+    write.csv(x=empty_data, file=where_to_save, row.names = FALSE)
+}
+
 
 # --------- SNAKEMAKE I/O -------------- # 
 cases_table     = read.csv(snakemake@input[["cases_table"]], sep=',')
@@ -107,83 +120,78 @@ genes_of_interest <- cases_of_interest[high_gscore &
                                        not_other,
                                        c('Hugo_Symbol', 'case_id', 'Consequence')]
 
- # get a table of genes with unique samples and sample count
-genes_to_test <- aggregate(case_id ~ Hugo_Symbol,
-                           genes_of_interest,
-                           function(x) length(unique(x)))
-
-colnames(genes_to_test) <- c('gene','count') 
-genes_to_test <- genes_to_test[order(-genes_to_test$count),] 
-  
-# Select genes with sample count > 5 and initialize results dataframe
-genes_to_test <- genes_to_test[genes_to_test$count >= 5, 'gene']
-
-
-if (length(genes_to_test) == 0){  
-    #TODO: Refactor this
-    # Generating empty dataframe for now, it won't crash downstream
-    empty_data <- data.frame('Hugo_Symbol' = "", 
-                             'adj_pval' = "",
-                             'log2fc' = "",
-                             'method' = "",
-                             'cases' = "",
-                             'bias' = "")
-   
-     write.csv(x=empty_data, file=where_to_save, row.names = FALSE)
+# Check if the dataframe is empty after filtering genes of interest
+if(dim(genes_of_interest)[1] > 0)
+{
+    # get a table of genes with unique samples and sample count
+    genes_to_test <- aggregate(case_id ~ Hugo_Symbol,
+                               genes_of_interest,
+                               function(x) length(unique(x)))
     
-}else{
-    print(paste(length(genes_to_test), 'genes will be tested for cnv DE'))
+    colnames(genes_to_test) <- c('gene','count') 
+    genes_to_test <- genes_to_test[order(-genes_to_test$count),] 
     
-    # Loop over genes of interest to perform deseqs
-    gtests <- foreach(gene=genes_to_test,
-                      .packages = c('DESeq2', 'edgeR'),
-                      .inorder = FALSE,
-                      .combine = 'rbind') %dopar% {
+    # Select genes with sample count > 5 
+    genes_to_test <- genes_to_test[genes_to_test$count >= 5, 'gene']
+    
+    # Check whether we have enough samples for DE
+    if (length(genes_to_test) > 0){  
         
-        # cases are those patients sharing a cnv_gain event
-        cases <- unique(genes_of_interest[genes_of_interest$Hugo_Symbol == gene,'case_id'])
-    
-        # get cases not carrying our gene of interest as a GoF or LoF
-        not_cases        <- cases_of_interest[!cases_of_interest$case_id %in% cases,]
-        valid_gene_alt   <- (not_cases$Hugo_Symbol == gene & not_cases$Consequence == 'Unknown')
-        gene_not_alt     <- not_cases$Hugo_Symbol != gene
-        controls         <- unique(not_cases[valid_gene_alt | gene_not_alt, 'case_id'])
-        bias             <- 0 # Whether cases were coerced to controls or not
-      
-          if(length(cases) > length(controls)){
-            cases <- sample(cases, size=length(controls))
-            bias  <- 1
-        }else{
-            controls = sample(controls, size = length(cases))
-        }
-    
-    total_samples <- length(cases) + length(controls)
-    
-    analysis_class <- 'unknown'
-    test_results   <- c(0,0)
-    
-    if(total_samples <= 10){
-        analysis_class <- 'deseq2'  
-        test_results <- perform_deseq2(data_matrix = expr_matrix,
-                                       samples = cases,
-                                       controls = controls,
-                                       gene = gene)
-    }else{
-        analysis_class <- 'limma'
-        test_results <- perform_limma(data_matrix = expr_matrix,
-                                      samples = cases,
-                                      controls = controls,
-                                      gene = gene)
-    }
-    # Return a row to later merge in results dataframe. 
-    gene <- as.character(gene)
-    gene_de_sum <- c(gene, test_results, analysis_class, length(cases), bias)
-    return(gene_de_sum)
-    }
-  
-  colnames(gtests) <- c('Hugo_Symbol', 'adj_pval', 'log2fc', 'method', 'cases', 'bias')
-  write.csv(x=as.data.frame(gtests), file=where_to_save, row.names = FALSE)
-}
+        print(paste(length(genes_to_test), 'genes will be tested for cnv DE'))
+        
+        # Loop over genes of interest to perform deseqs
+        gtests <- foreach(gene=genes_to_test,
+                          .packages = c('DESeq2', 'edgeR'),
+                          .inorder = FALSE,
+                          .combine = 'rbind') %dopar% {
+                              
+                              # cases are those patients sharing a cnv_gain event
+                              cases <- unique(genes_of_interest[genes_of_interest$Hugo_Symbol == gene,'case_id'])
+                              
+                              # get cases not carrying our gene of interest as a GoF or LoF
+                              not_cases        <- cases_of_interest[!cases_of_interest$case_id %in% cases,]
+                              valid_gene_alt   <- (not_cases$Hugo_Symbol == gene & not_cases$Consequence == 'Unknown')
+                              gene_not_alt     <- not_cases$Hugo_Symbol != gene
+                              controls         <- unique(not_cases[valid_gene_alt | gene_not_alt, 'case_id'])
+                              bias             <- 0 # Whether cases were coerced to controls or not
+                              
+                              if(length(cases) > length(controls)){
+                                  cases <- sample(cases, size=length(controls))
+                                  bias  <- 1
+                              }else{
+                                  controls = sample(controls, size = length(cases))
+                              }
+                              
+                              total_samples <- length(cases) + length(controls)
+                              
+                              analysis_class <- 'unknown'
+                              test_results   <- c(0,0)
+                              
+                              if(total_samples <= 10){
+                                  analysis_class <- 'deseq2'  
+                                  test_results <- perform_deseq2(data_matrix = expr_matrix,
+                                                                 samples = cases,
+                                                                 controls = controls,
+                                                                 gene = gene)
+                              }else{
+                                  analysis_class <- 'limma'
+                                  test_results <- perform_limma(data_matrix = expr_matrix,
+                                                                samples = cases,
+                                                                controls = controls,
+                                                                gene = gene)
+                              }
+                              # Return a row to later merge in results dataframe. 
+                              gene <- as.character(gene)
+                              gene_de_sum <- c(gene, test_results, analysis_class, length(cases), bias)
+                              return(gene_de_sum)
+                          }
+        
+        colnames(gtests) <- c('Hugo_Symbol', 'adj_pval', 'log2fc', 'method', 'cases', 'bias')
+        write.csv(x=as.data.frame(gtests), file=where_to_save, row.names = FALSE)
+        
+            #TODO: Refactor this and add reasons. Ex: "Could not perform DEG because of xxx"
+            }else{ save_dummy_dataframe(where_to_save) }
+}else{ save_dummy_dataframe(where_to_save) }
 
 
 
